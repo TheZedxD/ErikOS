095001e86d6306964c66698d7be5650de9b737f9
TheZedxD <108346257+TheZedxD@users.noreply.github.com>
2025-08-29 08:32:18 -0400

    Use requirements file and improve install scripts

--- install.bat
+++ install.bat
@@ -1,6 +1,8 @@
-@echo off
+@echo on
+cd /d "%~dp0"
 python -m venv .venv
 call .venv\Scripts\activate
-pip install --upgrade pip
-pip install Flask Pillow psutil
-echo Installed Flask/Pillow/psutil.
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+echo Install complete.
+pause
--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
+cd "$(dirname "$0")"
 python3 -m venv .venv
 source .venv/bin/activate
-pip install --upgrade pip
-pip install Flask Pillow psutil
-echo "Installed Flask/Pillow/psutil."
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+echo "Install complete."
